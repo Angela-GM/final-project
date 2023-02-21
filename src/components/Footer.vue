b0b9aa4a86a32ab4407263768125a07522a36d2c
Angela-GM <angelagm0112@gmail.com>
2023-02-21 16:01:43 +0100

    Footer terminado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,42 @@
 <template>
-  <h1>Footer</h1>
+  <footer>
+    <ul>
+      <li>
+        <a href="https://www.linkedin.com/in/angelagarciam82/" target="_black"
+          ><i class="fa-brands fa-linkedin"></i
+        ></a>
+      </li>
+      <li>
+        <a href="https://github.com/Angela-GM" target="_blank"
+          ><i class="fa-brands fa-github"></i
+        ></a>
+      </li>
+      <li>
+        <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+      </li>
+      <li>
+        <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+      </li>
+      <li>
+        <a href="mailto:angelagm0112@gmail.com" target="_blank"
+          ><i class="fa-solid fa-envelope"></i
+        ></a>
+      </li>
+    </ul>
+
+    <small
+      ><i class="fa-regular fa-copyright"></i> {{ currentYear }} Angela Garcia
+      Muñoz | FrontEnd Developer |
+      <a href="https://github.com/Angela-GM/final-project" target="_blank"
+        >Final Project Ironhack</a
+      >
+    </small>
+  </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const currentYear = ref(new Date().getFullYear());
+</script>
 
 <style></style>
